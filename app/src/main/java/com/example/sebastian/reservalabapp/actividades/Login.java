79d38e3e20d77ce838f5af550677d46ceb42b018
sebastian <seba.nobody@gnail.com>
2018-02-10 20:30:11 -0300

    commitdelacoshina

--- app/src/main/java/com/example/sebastian/reservalabapp/actividades/Login.java
+++ app/src/main/java/com/example/sebastian/reservalabapp/actividades/Login.java
@@ -1,15 +1,44 @@
 package com.example.sebastian.reservalabapp.actividades;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.sebastian.reservalabapp.R;
 
 public class Login extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout; //necesario para el  drawer layout
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setToolbar(); //instancio el action bar
+    }
+
+
+
+    //setea la barra de herramientas
+    private void setToolbar(){
+        Toolbar myToolbar =  findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home); //relaciona el icono en la barra
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //habilita el icono en la barra
+    }
+
+    //abre el menu lateral al apretar el boton sandwich
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                //abrir el menu lateral
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/sebastian/reservalabapp/actividades/MainActivity.java
+++ app/src/main/java/com/example/sebastian/reservalabapp/actividades/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.sebastian.reservalabapp.actividades;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -20,6 +22,9 @@ import android.widget.Toast;
 import com.example.sebastian.reservalabapp.R;
 import com.example.sebastian.reservalabapp.adaptadores.PagerAdapter;
 import com.example.sebastian.reservalabapp.fragments.DetailFragment;
+import com.example.sebastian.reservalabapp.fragments.menu_fragments.AlertFragment;
+import com.example.sebastian.reservalabapp.fragments.menu_fragments.EmailFragment;
+import com.example.sebastian.reservalabapp.fragments.menu_fragments.InfoFragment;
 import com.example.sebastian.reservalabapp.fragments.testFragment;
 
 import java.util.ArrayList;
@@ -30,15 +35,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     private DrawerLayout drawerLayout; //necesario para el  drawer layout
-
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         setToolbar(); //instancio el action bar
+        navigationView = findViewById(R.id.navView);
+        setFragmentByDefault();
 
         TabLayout tabLayout = findViewById(R.id.tab_menu);
         //agrego los tabs
@@ -98,8 +103,58 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        //ImageView imv = findViewById(R.id.user_picture);
-        //Picasso.with(getApplicationContext()).load(R.drawable.default_user).into(imv);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //menu item en donde hagamos click
+                boolean fragentTransaction = false;
+                Fragment fragment = null;
+                switch (item.getItemId()){
+
+                    case R.id.menu_login:
+                        fragment = new EmailFragment();
+                        fragentTransaction = true;
+                        break;
+                    case R.id.menu_email:
+                        fragment = new EmailFragment();
+                        fragentTransaction = true;
+                        break;
+                    case R.id.menu_alert:
+                        fragment = new AlertFragment();
+                        fragentTransaction = true;
+                        break;
+                    case R.id.menu_info:
+                        fragment = new InfoFragment();
+                        fragentTransaction = true;
+                        break;
+                }
+
+                if(fragentTransaction){
+                    changeFragment(fragment, item);
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+
+
+                return false;
+            }
+        });
+
+
+
+    }
+
+    private void changeFragment(Fragment fragment, MenuItem item){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame,fragment)
+                .commit();
+        item.setChecked(true);
+        getSupportActionBar().setTitle(item.getTitle());
+    }
+
+    private void setFragmentByDefault(){
+        changeFragment(new EmailFragment(),navigationView.getMenu().getItem(0) );
     }
 
     //setea la barra de herramientas
